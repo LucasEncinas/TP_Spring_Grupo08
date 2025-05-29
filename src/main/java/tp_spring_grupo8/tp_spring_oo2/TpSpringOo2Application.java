317f8fb45e9d2654b2e6385695c263b43df15817
LucasEncinas <lucas.encinas17@gmail.com>
2025-05-29 18:00:46 -0300

    first commit

---
+++ src/main/java/tp_spring_grupo8/tp_spring_oo2/TpSpringOo2Application.java
@@ -0,0 +1,13 @@
+package tp_spring_grupo8.tp_spring_oo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringOo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringOo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/tp_spring_grupo8/tp_spring_oo2/TpSpringOo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tp_spring_grupo8.tp_spring_oo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringOo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
