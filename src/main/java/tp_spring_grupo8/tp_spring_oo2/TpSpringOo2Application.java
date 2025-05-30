ed3d9f96fedbb0d224bf07dfd253f693dbf57265
Lucas Iván Cardozo <lucascardozo540@gmail.com>
2025-05-30 00:51:14 -0300

    Se solucionó el problema de las variables de entorno.

--- src/main/java/tp_spring_grupo8/tp_spring_oo2/TpSpringOo2Application.java
+++ src/main/java/tp_spring_grupo8/tp_spring_oo2/TpSpringOo2Application.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
 @EntityScan(basePackages = "entities")
-
+@SpringBootApplication
 public class TpSpringOo2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpSpringOo2Application.class, args);
+
+		
 	}
 
 }
